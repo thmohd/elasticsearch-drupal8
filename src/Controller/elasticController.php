61dcd127494533c7c67df7db9350c0951b6f5d53
Mohammad Althayabeh <althayabeh.mohammad@gmail.com>
2016-08-18 02:03:54 +0000

    Successful connection test  with Elasticsearch

--- src/Controller/elasticController.php
+++ src/Controller/elasticController.php
@@ -10,10 +10,28 @@
  */
 
 namespace Drupal\elasticsearch\Controller;
+require_once DRUPAL_ROOT . '/modules/custom/elasticsearch/vendor/autoload.php';
+
+use Elasticsearch\ClientBuilder;
 
 class elasticController{
 
   public function Index(){
+
+    $hosts = ['192.168.56.101:9200'];
+    $client = ClientBuilder::create()->setHosts($hosts)->build();
+
+
+    $params = [
+      'index'  => 'test_missing',
+      'type'   => 'test',
+      'id'     => 1,
+      'client' => [
+        'ignore' => 404
+      ]
+    ];
+    $test = $client->get($params);
+    print_r($test);
 
     return array(
       '#markup' => 'hello',
